aced78664255f3231c225a55173915c183c21bf7
D14LB3L0 <diegobdev2024@gmail.com>
2025-06-07 17:39:33 +0100

    added get and post method

--- Pages/Book.cshtml.cs
+++ Pages/Book.cshtml.cs
@@ -1,11 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using razorPage.Models;
 
 namespace razorPage.Pages
 {
     public class BookModel : PageModel
     {
+        private readonly MyDbContext _context;
+
+        public List<Book> Books { get; set; } = new List<Book>();
+
+        [BindProperty]
+        public Book NewBook { get; set; }
+
+        public BookModel(MyDbContext context)
+        {
+            _context = context;
+        }
+
         public void OnGet()
         {
+            Books = _context.Book.ToList();
+        }
+
+        public IActionResult OnPost()
+        {
+            _context.Book.Add(NewBook);
+
+            _context.SaveChanges();
+
+            return RedirectToPage();
         }
     }
 }
